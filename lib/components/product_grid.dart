d2082d47f0fe71560cf4e623640eb3191fa829d1
Lucas Reis <lucasreis_9@hotmail.com>
2023-04-14 15:57:15 -0300

    Extraindo Grid de Produtos

---
+++ lib/components/product_grid.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:loja_ii__flutter/components/product_item.dart';
+import 'package:loja_ii__flutter/models/product.dart';
+import 'package:loja_ii__flutter/models/product_list.dart';
+import 'package:provider/provider.dart';
+
+class ProductGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+    final List<Product> loadedProducts = provider.items;
+
+    return GridView.builder(
+      padding: EdgeInsets.all(10),
+      itemCount: loadedProducts.length,
+      itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
+      // Area dentro de algo que é scrollble/rolavel
+      // Com a qtd de itens fixos no eixo cruzado
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        // DOis produtos por linha
+        crossAxisCount: 2,
+        // Relacao de altura e largura
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,39 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:loja_ii__flutter/components/product_item.dart';
-import 'package:loja_ii__flutter/data/dummy_data.dart';
-import 'package:loja_ii__flutter/models/product.dart';
-import 'package:loja_ii__flutter/models/product_list.dart';
-import 'package:provider/provider.dart';
+import 'package:loja_ii__flutter/components/product_grid.dart';
 
 class ProductsOverviewPages extends StatelessWidget {
   ProductsOverviewPages();
 
   @override
   Widget build(BuildContext context) {
-    // <ProductList> é o tipo de provider que deseja obter
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> loadedProducts = provider.items;
-     
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha Loja'),
         centerTitle: true,
       ),
-      body: GridView.builder(
-        padding: EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
-        // Area dentro de algo que é scrollble/rolavel
-        // Com a qtd de itens fixos no eixo cruzado
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          // DOis produtos por linha
-          crossAxisCount: 2,
-          // Relacao de altura e largura
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      ),
+      body: ProductGrid(),
     );
   }
 }
