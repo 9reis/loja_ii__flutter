2c9074a5a003f9a0406deed844ab6efd0cead123
Lucas Reis <lucasreis_9@hotmail.com>
2023-04-15 18:42:26 -0300

    Remover item

--- lib/components/cart_item.dart
+++ lib/components/cart_item.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:loja_ii__flutter/models/cart.dart';
 import 'package:loja_ii__flutter/models/cart_item.dart';
+import 'package:provider/provider.dart';
 
 class CartItemWidget extends StatelessWidget {
   const CartItemWidget(this.cartItem, {Key? key}) : super(key: key);
@@ -10,22 +12,42 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Padding(
-              padding: EdgeInsets.all(5),
-              child: FittedBox(
-                child: Text('${cartItem.price}'),
+    return Dismissible(
+      key: ValueKey(cartItem.id),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+      ),
+      //Exclui o item do carrinho
+      onDismissed: (_) {
+        Provider.of<Cart>(context, listen: false)
+            .removeItem(cartItem.productId);
+      },
+      child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: EdgeInsets.all(5),
+                child: FittedBox(
+                  child: Text('${cartItem.price}'),
+                ),
               ),
             ),
+            title: Text(cartItem.name),
+            subtitle: Text('Total: R\$${cartItem.price * cartItem.quantity}'),
+            trailing: Text('${cartItem.quantity}x'),
           ),
-          title: Text(cartItem.name),
-          subtitle: Text('Total: R\$${cartItem.price * cartItem.quantity}'),
-          trailing: Text('${cartItem.quantity}x'),
         ),
       ),
     );
