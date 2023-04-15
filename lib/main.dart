07193d62bbf45971a401736d1f43df2b29ab0e70
Lucas Reis <lucasreis_9@hotmail.com>
2023-04-14 21:23:24 -0300

    MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loja_ii__flutter/models/cart.dart';
 import 'package:loja_ii__flutter/models/product_list.dart';
 import 'package:loja_ii__flutter/pages/counter_page.dart';
 import 'package:loja_ii__flutter/pages/product_detail_page.dart';
@@ -16,8 +17,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => ProductList(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ProductList(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Cart(),
+        ),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
