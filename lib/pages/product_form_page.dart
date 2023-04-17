4175c47b80723f5d00e2af34effdee98b3488a93
Lucas Reis <lucasreis_9@hotmail.com>
2023-04-16 21:51:16 -0300

    Campo de Entrada de URL

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -12,12 +12,29 @@ class ProductFormPage extends StatefulWidget {
 class _ProductFormPageState extends State<ProductFormPage> {
   final _priceFocus = FocusNode();
   final _descriptionFocus = FocusNode();
+  final _imageUrlFocus = FocusNode();
+  final _imageUrlController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    // Sempre que perder o foco ou ganhar o foco
+    // vai chamar o met
+    _imageUrlFocus.addListener(updateImage);
+  }
 
   @override
   void dispose() {
     super.dispose();
     _priceFocus.dispose();
     _descriptionFocus.dispose();
+    _imageUrlFocus.removeListener(updateImage);
+    _imageUrlFocus.dispose();
+  }
+
+  // Para dar um refresh na UI
+  void updateImage() {
+    setState(() {});
   }
 
   @override
@@ -51,10 +68,43 @@ class _ProductFormPageState extends State<ProductFormPage> {
               TextFormField(
                 decoration: InputDecoration(labelText: 'Descrição'),
                 // Vai para o px input
-                textInputAction: TextInputAction.next,
                 focusNode: _descriptionFocus,
                 keyboardType: TextInputType.multiline,
                 maxLines: 3,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Url da Imagem'),
+                      focusNode: _imageUrlFocus,
+                      keyboardType: TextInputType.url,
+                      // Submete o form ao clicar no enter
+                      textInputAction: TextInputAction.done,
+                      // Para ter acesso ao valor do texto
+                      controller: _imageUrlController,
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    margin: EdgeInsets.only(top: 10, left: 10),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1,
+                      ),
+                    ),
+                    child: _imageUrlController.text.isEmpty
+                        ? Text('Informa a URL')
+                        : FittedBox(
+                            child: Image.network(_imageUrlController.text),
+                            fit: BoxFit.cover,
+                          ),
+                  ),
+                ],
               ),
             ],
           ),
