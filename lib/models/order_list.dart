439f6ca4150895a5a72d891436e5115320dd44c7
Lucas Reis <lucasreis_9@hotmail.com>
2023-04-18 20:54:37 -0300

    Armazenando Pedidos no Firebase

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -1,11 +1,14 @@
 // Gerencia todos os pedidos
 // Gera um notificação sempre que houver mudança
 
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:loja_ii__flutter/models/cart.dart';
 import 'package:loja_ii__flutter/models/order.dart';
+import 'package:loja_ii__flutter/utils/constants.dart';
 
 class OrderList with ChangeNotifier {
   List<Order> _items = [];
@@ -18,14 +21,38 @@ class OrderList with ChangeNotifier {
     return _items.length;
   }
 
-  void addOrder(Cart cart) {
+  Future<void> addOrder(Cart cart) async {
+    final date = DateTime.now();
+
+    final res = await http.post(
+      // Recebe a coleção que deseja armazenar os dados
+      Uri.parse('${Constants.ORDER_BASE_URL}.json'),
+      body: jsonEncode(
+        {
+          'total': cart.totalAmount,
+          'date': date.toIso8601String(),
+          'products': cart.items.values
+              .map((cartItem) => {
+                    'id': cartItem.id,
+                    'productId': cartItem.productId,
+                    'name': cartItem.name,
+                    'quantity': cartItem.quantity,
+                    'price': cartItem.price
+                  })
+              .toList(),
+        },
+      ),
+    );
+    // Pega o id de cada item no banco
+    final id = jsonDecode(res.body)['name'];
+
     _items.insert(
       0,
       Order(
-        id: Random().nextDouble().toString(),
+        id: id,
         total: cart.totalAmount,
         products: cart.items.values.toList(),
-        date: DateTime.now(),
+        date: date,
       ),
     );
     notifyListeners();
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -46,21 +46,7 @@ class CartPage extends StatelessWidget {
                     ),
                   ),
                   Spacer(),
-                  TextButton(
-                    child: Text('COMPRAR'),
-                    onPressed: () {
-                      Provider.of<OrderList>(
-                        context,
-                        listen: false,
-                      ).addOrder(cart);
-
-                      cart.clear();
-                    },
-                    style: TextButton.styleFrom(
-                      textStyle:
-                          TextStyle(color: Theme.of(context).primaryColor),
-                    ),
-                  ),
+                  CartButton(cart: cart),
                 ],
               ),
             ),
@@ -74,5 +60,46 @@ class CartPage extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class CartButton extends StatefulWidget {
+  const CartButton({
+    super.key,
+    required this.cart,
+  });
+
+  final Cart cart;
+
+  @override
+  State<CartButton> createState() => _CartButtonState();
+}
+
+class _CartButtonState extends State<CartButton> {
+  bool _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return _isLoading
+        ? CircularProgressIndicator()
+        : TextButton(
+            child: Text('COMPRAR'),
+            onPressed: widget.cart.itemnsCount == 0
+                ? null
+                : () async {
+                    setState(() => _isLoading = true);
+
+                    await Provider.of<OrderList>(
+                      context,
+                      listen: false,
+                    ).addOrder(widget.cart);
+                    widget.cart.clear();
+
+                    setState(() => _isLoading = false);
+                  },
+            style: TextButton.styleFrom(
+              textStyle: TextStyle(color: Theme.of(context).primaryColor),
+            ),
+          );
   }
 }
