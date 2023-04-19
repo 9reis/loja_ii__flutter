823e75fbb811fb71f73df9356f4644408eee3968
Lucas Reis <lucasreis_9@hotmail.com>
2023-04-18 22:57:39 -0300

    Tela de Autenticacao #2

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -17,12 +19,47 @@ class AuthPage extends StatelessWidget {
                 Color.fromRGBO(1, 121, 109, 1),
                 Color.fromRGBO(33, 160, 111, 0.9),
               ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
           ),
         ),
         Container(
           width: double.infinity,
-          child: Column(children: []),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 70,
+                ),
+                // Cascade Operator
+                //transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-5.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.deepOrange.shade900,
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 12,
+                      color: Colors.black26,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: Text(
+                  'Minha Loja',
+                  style: TextStyle(
+                    fontSize: 45,
+                    fontFamily: 'Anton',
+                    color:
+                        Theme.of(context).accentTextTheme.headlineMedium?.color,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ],
     ));
